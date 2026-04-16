19d0449fe34a57734b2d717d2ab7fba2447b54c4
Apple <atharva.kshirsagar2810@gmail.com>
2026-04-16 12:26:56 +0530

    Connected Supabase to our project

---
+++ FleetManagementSystem/Supabase/SupabaseConfig.swift
@@ -0,0 +1,14 @@
+//
+//  SupabaseConfig.swift
+//  FleetManagementSystem
+//
+//  Created by Apple on 16/04/26.
+//
+
+import SwiftUI
+
+struct SupabaseConfig {
+    static let url = URL(string: "https://qisdvwaldlghndrudbvr.supabase.co")!
+    static let anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpc2R2d2FsZGxnaG5kcnVkYnZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNDUxMjQsImV4cCI6MjA5MTgyMTEyNH0.NF5JC8_Attne5ULy-iuheZv7Oy2zl4LHrz9zFAhfJLk"
+}
+ 
