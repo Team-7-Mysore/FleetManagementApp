79adf2033e5fa5f4d302a893bb70b35e1c708ec2
Kandyyy <kingkandy016@gmail.com>
2026-04-16 09:28:12 +0530

    Updated file structure

---
+++ FleetManagementSystem/Features/FleetManager/Dashboard/Views/FleetManagerDashboardView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FleetManagementSystem
+//
+//  Created by harshwardhan patil on 15/04/26.
+//
+
+import SwiftUI
+
+struct FleetManagerDashboardView: View {
+    var body: some View {
+        Color.clear
+    }
+}
+
+struct FleetManagerDashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FleetManagerDashboardView()
+    }
+}
